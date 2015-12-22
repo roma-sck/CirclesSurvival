182190b385051ce5127c2fc4607f1649a44398b9
roma.sck <roma.sck@gmail.com>
2015-12-22 11:37:31 +0200

    transfer values to dimens and strings

--- app/src/main/java/com/example/sck/circles_survival/fragments/GameSettingsFragment.java
+++ app/src/main/java/com/example/sck/circles_survival/fragments/GameSettingsFragment.java
@@ -109,7 +109,7 @@ public class GameSettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_colors, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
 }
