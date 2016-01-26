975318943f13688cbf0469fe0abd05992585a445
roma.sck <roma.sck@gmail.com>
2016-01-26 16:33:39 +0200

    add how to play button

--- app/src/main/java/com/example/sck/circles_survival/fragments/HowToPlayFragment.java
+++ app/src/main/java/com/example/sck/circles_survival/fragments/HowToPlayFragment.java
@@ -1,10 +1,12 @@
 package com.example.sck.circles_survival.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.sck.circles_survival.R;
 
@@ -12,7 +14,18 @@ public class HowToPlayFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_how_to_play, container, false);
+        View view = inflater.inflate(R.layout.fragment_how_to_play, container, false);
+        Button btnHowToPlay = (Button)getActivity().findViewById(R.id.btnHowToPlay);
+        btnHowToPlay.setOnClickListener(btnHowToPlayOnClickListener);
+        return view;
     }
+
+    View.OnClickListener btnHowToPlayOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(getActivity(), ImagePagerViewActivity.class));
+        }
+    };
+
 }
 
